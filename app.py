ebd7565c6b239d869e3b792687c55c6110cb56ad
Omair Hassan <hmair088@gmail.com>
2022-07-20 12:38:02 -0500

    .

--- app.py
+++ app.py
@@ -3,7 +3,6 @@ from flask import Flask, render_template
 # Creates a flask instance
 app = Flask(__name__)
 
-@app.route('/')
-
+@app.route("/",methods=["POST","GET"])
 def index():
 	return render_template(home.html)
